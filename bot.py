80e7674ce549e033c819a66937c291ba4e1d70d7
Saori Tamaki <s-001086@student.german-uds.de>
2026-05-24 06:09:57 +0000

    Retested Slack integration without hardcoded token

--- bot.py
+++ bot.py
@@ -1,1 +1,8 @@
+import os
+from slack_sdk import WebClient
 
+
+SLACK_TOKEN = os.environ["SLACK_TOKEN"]
+
+client = WebClient(token=SLACK_TOKEN)
+client.chat_postMessage(channel='#all-team-charlie-workspace',text='Test successful: Python was able to send a message to the Slack app channel')
