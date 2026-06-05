4b06cf9f6b925b6f9da63fa56adc6aae1aa3286c
Saori Tamaki <s-001086@student.german-uds.de>
2026-06-05 23:04:31 +0800

    Add 3-day reminder logic

--- reminder_bot/slack.py
+++ reminder_bot/slack.py
@@ -1,1 +1,35 @@
-print("Slack dot py is here")
+from dotenv import load_dotenv
+from course_dates import due_dates
+
+import os
+from slack_sdk import WebClient
+
+load_dotenv("creds.env")
+
+SLACK_TOKEN = os.environ["SLACK_TOKEN"]
+client = WebClient(token=SLACK_TOKEN)
+
+
+from datetime import datetime
+today = datetime.now()
+
+message = ""
+for d in due_dates:
+    days_left = (d.date.date() - today.date()).days
+    if days_left == 3:
+
+        if message == "":
+            message = "📅 Upcoming Deadline\n\n\n"
+            message += "📘 Course: Python Capstone\n\n"
+
+        message += f" 📌 Module: {d.title}\n"
+        message += f" 🗓️ Due: {d.format_date()}\n\n"    
+
+
+client.chat_postMessage(
+    channel="#python_capstone-deadline-reminders",
+    text=message
+)
+
+print("pcoming Deadlines test message sent successfully")
+print(message)
